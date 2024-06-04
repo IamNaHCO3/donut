a2a0f0a2511f9a28b925d70750708533b4d5a241
IamNaHCO3 <iamnahco3@gmail.com>
2024-06-04 16:05:57 +0800

    refactor: add space as the char of lightest color

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ fn main() {
     let frame_rate = 30;
 
     // 显示大小主要取决于zoom以及major_radius和minor_radius的比例
-    let zoom = 15.0;
+    let zoom = 20.0;
     let major_radius = 40.0;
     let minor_radius = 5.0;
-    let screen_size = [30, 40];
+    let screen_size = [30, 100];
     let granularity = (screen_size[0] as f64 + screen_size[1] as f64 * 0.8) as i64;
 
     let mut rng = rand::thread_rng();
@@ -22,7 +22,7 @@ fn main() {
     let donut = donut::Donut::new(major_radius, minor_radius, zoom, rotation_speed, rotation_angle, granularity, screen_size);
     match donut {
         Ok(mut donut) => {
-            let canva = canvas::Canvas::new(vec!['.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@']);
+            let canva = canvas::Canvas::new(vec![' ', '.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@']);
             print!("\x1b[?25l"); // 隐藏光标
 
             for _i in 0..100 {
@@ -33,8 +33,6 @@ fn main() {
                 // 暂停
                 thread::sleep(Duration::from_millis(1000 / frame_rate));
             }
-
-            println!("{:?}", rotation_speed);
 
         },
         Err(e) => {
